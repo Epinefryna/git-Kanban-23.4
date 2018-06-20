ef8c50a47129db86142738ba94e41d1e565bab3e
Dawid Janota <6dawid.janota@wp.pl>
2018-06-20 05:51:35 +0200

    modified

--- server/routes/lane.routes.js
+++ server/routes/lane.routes.js
@@ -1,11 +1,21 @@
 import { Router } from 'express';
 import * as LaneController from '../controllers/lane.controller';
+import noteRouter from './note.routes';
 
 const router = new Router();
 
-router.route('/lanes').post(LaneController.addLane);
+// Get all Lanes
 router.route('/lanes').get(LaneController.getLanes);
+
+// Add a new Lane
+router.route('/lanes').post(LaneController.addLane);
+
+// Delete a lane by laneId
 router.route('/lanes/:laneId').delete(LaneController.deleteLane);
-router.route('/lanes/:laneId').patch(LaneController.editLane);
+
+//Update a line name
+router.route('/lanes/:laneId').put(LaneController.updateLane);
+
+router.use('/lanes/:laneId/', noteRouter);
 
 export default router;
--- server/routes/note.routes.js
+++ server/routes/note.routes.js
@@ -1,10 +1,15 @@
 import { Router } from 'express';
 import * as NoteController from '../controllers/note.controller';
 
-const router = new Router();
+const router = new Router({mergeParams: true});
 
+// Add a new Note
 router.route('/notes').post(NoteController.addNote);
+
+//Delete a Note
 router.route('/notes/:noteId').delete(NoteController.deleteNote);
-router.route('/notes/:noteId').patch(NoteController.editNote);
+
+//Update a Note
+router.route('/notes/:noteId').put(NoteController.updateNote);
 
 export default router;
